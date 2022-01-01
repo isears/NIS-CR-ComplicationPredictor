3b03eaebc38c25c5d1433d1daeff82d0ec4bea9b
raceee <race.peterson@gmail.com>
2021-12-31 22:53:04 -0700

    table 2 generated

--- pickle_vis.py
+++ pickle_vis.py
@@ -1,13 +1,34 @@
 import pickle
-from modeling.util import CvResult
+import docx
+  
+doc = docx.Document()
+  
+doc.add_heading('Table 2', 0)
+
+table = doc.add_table(rows=9 , cols=5)
+
+row = table.rows[0].cells
+row[0].text = 'Algo Name'
+row[1].text = 'Accuracy'
+row[2].text = 'Specificity'
+row[3].text = 'Sensitivity'
+row[4].text = 'AUC'
+
 
 with open('test_results.pkl', 'rb') as f:
     while True:
         try:
             a = pickle.load(f)
-            # print(a.folds[0].trained_classifier.__class__.__name__)
-            print(type(a.__dict__['folds'][0])) # <class 'modeling.util.SingleFoldResult'>
-            # print(a.auc_avg())
+            auc = a.auc_avg()
+            keys_ = a.__dict__['folds'][0].__dict__ 
+            keys_["auc"] = auc
+            row = table.add_row().cells
+            row[0].text = keys_["trained_classifier"].__class__.__name__
+            row[1].text = str("{:.2f}".format(keys_["accuracy"]))
+            row[2].text = str("{:.2f}".format(keys_["specificity"]))
+            row[3].text = str("{:.2f}".format(keys_["sensitivity"]))
+            row[4].text = str("{:.2f}".format(keys_["auc"]))
             print("-----------")
         except EOFError:
-                break
+                break 
+doc.save("table2.docx")
