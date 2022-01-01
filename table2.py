54762733cdfb1ee000effcb0e90f5eec69fc1a00
raceee <race.peterson@gmail.com>
2021-12-31 23:12:36 -0700

    table edits

--- table2.py
+++ table2.py
@@ -5,28 +5,31 @@ doc = docx.Document()
   
 doc.add_heading('Table 2', 0)
 
-table = doc.add_table(rows=9 , cols=5)
+table = doc.add_table(rows=1 , cols=6)
 
 row = table.rows[0].cells
 row[0].text = 'Algo Name'
-row[1].text = 'Accuracy'
-row[2].text = 'Specificity'
-row[3].text = 'Sensitivity'
-row[4].text = 'AUC'
+row[1].text = "Task"
+row[2].text = 'Accuracy'
+row[3].text = 'Specificity'
+row[4].text = 'Sensitivity'
+row[5].text = 'AUC'
 
 
 with open('test_results.pkl', 'rb') as f:
     while True:
         try:
             a = pickle.load(f)
+            tar_ = a.__dict__["prediction_target"]
             keys_ = a.__dict__['folds'][0].__dict__ 
             keys_["auc"] = a.auc_avg()
             row = table.add_row().cells
             row[0].text = keys_["trained_classifier"].__class__.__name__
-            row[1].text = str("{:.2f}".format(keys_["accuracy"]))
-            row[2].text = str("{:.2f}".format(keys_["specificity"]))
-            row[3].text = str("{:.2f}".format(keys_["sensitivity"]))
-            row[4].text = str("{:.2f}".format(keys_["auc"]))
+            row[1].text = tar_
+            row[2].text = str("{:.2f}".format(keys_["accuracy"]))
+            row[3].text = str("{:.2f}".format(keys_["specificity"]))
+            row[4].text = str("{:.2f}".format(keys_["sensitivity"]))
+            row[5].text = str("{:.2f}".format(keys_["auc"]))
         except EOFError:
                 break 
 table.style = 'Medium Grid 3 Accent 1'
