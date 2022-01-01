5bc081e2dd3621aaa085ba19e3078c31527db691
raceee <race.peterson@gmail.com>
2021-12-31 22:59:55 -0700

    style on table error

--- pickle_vis.py
+++ pickle_vis.py
@@ -19,16 +19,15 @@ with open('test_results.pkl', 'rb') as f:
     while True:
         try:
             a = pickle.load(f)
-            auc = a.auc_avg()
             keys_ = a.__dict__['folds'][0].__dict__ 
-            keys_["auc"] = auc
+            keys_["auc"] = a.auc_avg()
             row = table.add_row().cells
             row[0].text = keys_["trained_classifier"].__class__.__name__
             row[1].text = str("{:.2f}".format(keys_["accuracy"]))
             row[2].text = str("{:.2f}".format(keys_["specificity"]))
             row[3].text = str("{:.2f}".format(keys_["sensitivity"]))
             row[4].text = str("{:.2f}".format(keys_["auc"]))
-            print("-----------")
         except EOFError:
                 break 
+table.style = 'Medium Grid 3 Accent 1'
 doc.save("table2.docx")
