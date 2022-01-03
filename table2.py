bdd333407fcbf672475644ae05d438f6dc76284e
raceee <race.peterson@gmail.com>
2022-01-03 13:24:18 -0700

    avg metrics

--- table2.py
+++ table2.py
@@ -19,14 +19,13 @@ with open('test_results.pkl', 'rb') as f:
             a = pickle.load(f)
             tar_ = a.__dict__["prediction_target"]
             keys_ = a.__dict__['folds'][0].__dict__ 
-            keys_["auc"] = a.auc_avg()
             row = table.add_row().cells
-            row[0].text = keys_["trained_classifier"].__class__.__name__
+            row[0].text = a.__dict__['folds'][0].__dict__["trained_classifier"].__class__.__name__
             row[1].text = tar_
-            row[2].text = str("{:.2f}".format(keys_["accuracy"]))
-            row[3].text = str("{:.2f}".format(keys_["specificity"]))
-            row[4].text = str("{:.2f}".format(keys_["sensitivity"]))
-            row[5].text = str("{:.2f}".format(keys_["auc"]))
+            row[2].text = str("{:.2f}".format(a.acc_avg())) # accuracy
+            row[3].text = str("{:.2f}".format(a.specificity_avg())) # specificity
+            row[4].text = str("{:.2f}".format(a.sensitivity_avg())) # sensitivity
+            row[5].text = str("{:.2f}".format(a.auc_avg())) # auc
         except EOFError:
                 break 
 table.style = 'Medium Grid 3 Accent 1'
