6d7902601751aba9f0686e2025dd963a987d6b2c
raceee <race.peterson@gmail.com>
2022-05-17 11:16:20 -0600

    #12 std reported

---
+++ meta_stats.py
@@ -0,0 +1,16 @@
+'''
+Created by @raceee on request of the standard deviation of the data. This script can be a place for all on demand stats on the project
+'''
+
+import pandas as pd
+import yaml
+
+with open("localconfig.yaml", "r") as yamlfile:
+    data = yaml.load(yamlfile)
+
+def get_std(df):
+    return df.std()
+
+if __name__ == "__main__":
+    med_df = pd.read_csv(data["data_path"])
+    print(get_std(med_df))
