6bc5761df129d6ee7675bb806687fbadae7f7eb7
Isaac Sears <isaac.j.sears@gmail.com>
2023-05-27 08:18:24 -0400

    Revert "Merge in CCI compare code"

--- preprocessingMain.py
+++ preprocessingMain.py
@@ -5,23 +5,17 @@ import preprocessing
 from concurrent.futures import ProcessPoolExecutor
 
 if __name__ == '__main__':
-    executor = ProcessPoolExecutor(max_workers=4)
+    executor = ProcessPoolExecutor(max_workers=2)
     lap_filtered = pd.read_csv(f"{util.SETTINGS['cache_path']}/lap_filtered.csv", low_memory=False)
     open_filtered = pd.read_csv(f"{util.SETTINGS['cache_path']}/open_filtered.csv", low_memory=False)
 
     lap_future = executor.submit(preprocessing.do_preprocessing, lap_filtered)
     open_future = executor.submit(preprocessing.do_preprocessing, open_filtered)
-    lap_cci_future = executor.submit(preprocessing.do_cci_preprocessing, lap_filtered)
-    open_cci_future = executor.submit(preprocessing.do_cci_preprocessing, open_filtered)
 
     lap_preprocessed = lap_future.result()
     open_preprocessed = open_future.result()
-    lap_cci_preprocessed = lap_cci_future.result()
-    open_cci_preprocessed = open_cci_future.result()
 
     lap_preprocessed.to_csv(f"{util.SETTINGS['cache_path']}/lap_preprocessed.csv", index=False)
     open_preprocessed.to_csv(f"{util.SETTINGS['cache_path']}/open_preprocessed.csv", index=False)
-    lap_cci_preprocessed.to_csv(f"{util.SETTINGS['cache_path']}/lap_cci_preprocessed.csv", index=False)
-    open_cci_preprocessed.to_csv(f"{util.SETTINGS['cache_path']}/open_cci_preprocessed.csv", index=False)
 
     print('[+] Preprocessing completed successfully')
