8a23182e3490d13a86c10d17c0066c3b4387b7c6
Race Peterson <race.peterson@gmail.com>
2023-06-13 13:13:19 -0600

    attempt at image saving

--- shap_figure.py
+++ shap_figure.py
@@ -7,6 +7,7 @@ import pickle
 from skorch import NeuralNetBinaryClassifier
 from skorch.callbacks import EarlyStopping
 from util import labels
+import matplotlib.pyplot as plt
 
 # get datas
 lap_data = "/Users/racepeterson/NIS-CR-ComplicationPredictor/open_dir/lap_preprocessed.csv"
@@ -17,17 +18,23 @@ lap_data = torch.from_numpy(lap_data[features].to_numpy()).float()
 open_data = torch.from_numpy(pd.read_csv(open_data).drop(columns=["APRDRG_Risk_Mortality"])[features].to_numpy()).float()
 
 # get model
-with open('nn_state.pkl',"rb") as f:
-    test_model = pickle.load(f)
-test_model.initialize()
-print("model loaded")
+# LAZY!!
+names = ["open_LOS_nn_state.pkl", "open_DIED_nn_state.pkl", "open_anastomotic_leak_nn_state.pkl","lap_LOS_nn_state.pkl", "lap_DIED_nn_state.pkl", "lap_anastomotic_leak_nn_state.pkl"]
+for model_name in names:
+    with open(model_name,"rb") as f:
+        test_model = pickle.load(f)
+    test_model.initialize()
+    print("model loaded")
 
-# .explainer()
-explainer = shap.KernelExplainer(test_model.predict, np.asarray(lap_data))
-sampled = shap.utils.sample(lap_data, nsamples=100, random_state=0)
-shap_values = explainer.shap_values(np.asarray(sampled))
-print("shap_values made")
+    # .explainer()
+    explainer = shap.KernelExplainer(test_model.predict, np.asarray(lap_data))
+    sampled = shap.utils.sample(lap_data, random_state=0)
+    shap_values = explainer.shap_values(np.asarray(sampled))
+    print("shap_values made")
 
-# # beswarm plot
-# print("Starting Beeswarm")
-# shap.plots.beeswarm(shap_values)
+    # # beswarm plot
+    print("Starting Beeswarm")
+    shap.plots.beeswarm(shap_values, color=plt.get_cmap("cool"), show=False)
+    image_name = model_name[:-4] + "image.png"
+    plt.savefig(image_name)
+    break
